e3d73fe2e35c059cc40c65dc062961727af6970d
Atharv Kolekar <kolekar.atharv1812@gmail.com>
2025-03-07 16:38:23 +0530

    APp.jsx file modified there was error in routes so fixed it

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,8 +12,8 @@ function App() {
 			<BrowserRouter>
 				<Navbar />
 				<Routes>
-					<Route path="/" element={user ? <Home /> : <Navigate to="/lol" />} />
-					<Route path="/signup" element={!user ? <Login /> : <Navigate to="/" />} />
+					<Route path="/" element={user ? <Home /> : <Navigate to="/login" />} />
+					<Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
 					<Route path="/signup" element={!user ? <Signup /> : <Navigate to="/" />} />
 				</Routes>
 			</BrowserRouter>
